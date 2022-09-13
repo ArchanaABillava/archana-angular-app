26da651fcafe28785ae52052ac8b062cf814987c
ArchanaABillava <Archana.Billava@evryindia.in>
2022-09-13 21:25:42 +0530

    Initial commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'archana-angular-app'`, () => {
+  it(`should have as title 'my-te-angular-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('archana-angular-app');
+    expect(app.title).toEqual('my-te-angular-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('archana-angular-app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('my-te-angular-app app is running!');
   });
 });
